2dce7133902f94502e49afe21c7ad0115a29b121
YARIKVOIN <isip_ya.yu.andreev@mpt.ru>
2023-01-30 16:46:57 +0300

    обновил код

--- PR2.py
+++ PR2.py
@@ -3,7 +3,10 @@ years = int(input())
 result = 0
 month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 if(years % 4 == 0):
-    result += 11
+    if(years % 100 == 0 and years % 400 != 0):
+        result = 0
+    else:
+        result += 11
 for i in range(len(month)):
     day = month[i]
     number = 1
