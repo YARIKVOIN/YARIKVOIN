39742db9f816be4d0dbafe3ca378201ba2eefcb1
YARIKVOIN <isip_ya.yu.andreev@mpt.ru>
2023-01-30 15:52:10 +0300

    PYTHON

--- untitled2.py
+++ untitled2.py
@@ -1,17 +1,17 @@
 print("Введите год: ")
-year = int(input())
-summa = 0
-days = {1: 31, 2: 28, 3: 31, 4: 30, 5: 31, 6: 30, 7: 31, 8: 31, 9: 30, 10: 31, 11: 30, 12: 31}
-if(year % 4 == 0):
-    summa += 1
-for x in range(1, len(days)+1):
-    day = days[x]
-    i = 1
-    while day >= i:
-        if(i >= 10):
-            stroka = str(i)
-            summa += int(stroka[0]) + int(stroka[1])
+years = int(input())
+result = 0
+month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+if(years % 4 == 0):
+    result += 11
+for i in range(len(month)):
+    day = month[i]
+    number = 1
+    while day >= number:
+        if(number >= 10):
+            perevod = str(number)
+            result += int(perevod[0]) + int(perevod[1])
         else:
-            summa += i
-        i += 1
-print(summa)
+            result += number
+        number += 1
+print(result)
